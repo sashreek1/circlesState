10fd53e045fffdc11ecead9e9f3f49095f2b92ac
sashreek1 <45600974+sashreek1@users.noreply.github.com>
2019-12-14 14:27:20 +0530

    Update circle_script2.0.py

--- circle_script2.0.py
+++ circle_script2.0.py
@@ -23,16 +23,16 @@ def run_gui ():
     root = Tk()
     root.geometry("500x250+900+300")
     root.resizable(False, False)
-    Label(root, text='Enter radius of circle 1 : (cm)', font=26).place(x=20,y=50)
+    Label(root, text='Enter radius of circle 1 : (px)', font=26).place(x=20,y=50)
     rad1 = Entry(root,width=10)
     rad1.place(x=260,y=50)
-    Label(root, text='Enter radius of circle 2 : (cm)', font=26).place(x=20, y=100)
+    Label(root, text='Enter radius of circle 2 : (px)', font=26).place(x=20, y=100)
     rad2 = Entry(root, width=10)
     rad2.place(x=260, y=100)
-    Label(root, text='Enter distance between the 2 circles : (cm)', font=26).place(x=20, y=150)
+    Label(root, text='Enter distance between the 2 circles : (px)', font=26).place(x=20, y=150)
     dist = Entry(root, width=10)
     dist.place(x=370, y=150)
-    u = Button(root, text="Submit", command=lambda: task(int(int(rad1.get())*37.7952755906), int(int(rad2.get())*37.7952755906), int(int(dist.get())*37.7952755906)))
+    u = Button(root, text="Submit", command=lambda: task(int(rad1.get()), int(rad2.get()), int(dist.get())))
     u.place(x=200, y=200)
     Label(root, text='note : all distances must be positive', font="Times_New_Roman 12 italic").place(x=20, y=5)
     root.mainloop()
@@ -43,6 +43,8 @@ def task(rad1,rad2,dist):
     circ_disp(700+(dist//2),500,rad2,blue)
     if rad2+rad1 == dist:
         message_display(green,(100,100),"The circles are tangential")
+    elif abs(rad1-rad2) == dist:
+        message_display(green, (100, 100), "The circles are tangential")
     elif dist ==0:
         message_display(green, (100, 100), "The circles are concentric")
     elif (rad2+rad1 < dist and dist!=0)or(min(rad1,rad2)>dist):
